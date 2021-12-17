ecb81742d80f0cc375ca975875dcfcab47ab5753
Alex Psitos <apsitos08@gmail.com>
2021-12-17 14:22:25 -0500

    Add model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  'name' => "Alex",
+  'years_coding' => 0,
+  'favorite_movie' => "Predator",
+  'programs_written' => 1,
+  'eager_to_learn' => true,
+  'currently_employed' => true,
+  'pets' => ["Olive", "Ozzy"],
+  'hobbies' => ["guitar", "mixing music"]
+}
