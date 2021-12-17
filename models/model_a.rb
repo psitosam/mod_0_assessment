84a20967836f062aa613ea73f224694293a33176
Alex Psitos <apsitos08@gmail.com>
2021-12-17 14:09:25 -0500

    Add model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  'name' => 'T rex',
+  'size' => 'extra large',
+  'weight' => 15000,
+  'teeth' => 60,
+  'meat_eater' => true,
+  'aggressive' => true,
+  'nicknames' => ["big boi", "king"],
+  'locations' => ["Montana", "Texas"] 
+}
