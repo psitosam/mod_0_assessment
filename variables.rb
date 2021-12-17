4da1d92e7193cae6d08bfb9b88b1bb819efd52af
Alex Psitos <apsitos08@gmail.com>
2021-12-17 13:54:47 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Blazing Saddles"
+is_entertaining = true
+movie = "Willow"
+num_4 = 4
+num_6 = 6
+product = "#{num_4} * #{num_6}"
+favorite_foods = ["steak", "pasta", "pizza"]
+favorite_foods.delete_at(0)
+p movie.length
